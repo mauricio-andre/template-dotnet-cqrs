93a4de07332dc323d81ab25a4b8d8ef5a1d2da45
Mauricio <mauricio.r.andre@gmail.com>
2025-02-20 15:50:23 -0300

    feat: implementado http 409 para conflito de entidade

--- cqrs-project/src/Apps/CqrsProject.App.RestServer/Filters/ExceptionFilter.cs
+++ cqrs-project/src/Apps/CqrsProject.App.RestServer/Filters/ExceptionFilter.cs
@@ -34,27 +34,47 @@ public class ExceptionFilter : IExceptionFilter
                         grp => GetNormalizedKey(grp.Key),
                         grp => grp.Select(x => x.ErrorMessage).ToArray()
                     ),
-                traceId = Activity.Current?.Id ?? context.HttpContext.TraceIdentifier
+                traceId = Activity.Current?.TraceId.ToString() ?? context.HttpContext.TraceIdentifier
             });
 
             context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
             context.ExceptionHandled = true;
         }
-        else if (context.Exception is BusinessException BusinessException)
+        else if (context.Exception is DuplicatedEntityException duplicatedEntityException)
+        {
+            context.Result = new BadRequestObjectResult(new
+            {
+                type = "https://tools.ietf.org/html/rfc9110#section-15.5.10",
+                title = _stringLocalizer["message:validation:businessExceptionTitle"].Value,
+                status = StatusCodes.Status409Conflict,
+                detail = duplicatedEntityException.Message,
+                instance = context.HttpContext.Request.Path.Value,
+                errors = duplicatedEntityException.Errors
+                    .ToDictionary(
+                        item => GetNormalizedKey(item.Key),
+                        item => item.Value
+                    ),
+                traceId = Activity.Current?.TraceId.ToString() ?? context.HttpContext.TraceIdentifier
+            });
+
+            context.HttpContext.Response.StatusCode = StatusCodes.Status409Conflict;
+            context.ExceptionHandled = true;
+        }
+        else if (context.Exception is BusinessException businessException)
         {
             context.Result = new BadRequestObjectResult(new
             {
                 type = "https://tools.ietf.org/html/rfc9110#section-15.5.1",
                 title = _stringLocalizer["message:validation:businessExceptionTitle"].Value,
                 status = StatusCodes.Status400BadRequest,
-                detail = BusinessException.Message,
+                detail = businessException.Message,
                 instance = context.HttpContext.Request.Path.Value,
-                errors = BusinessException.Errors
+                errors = businessException.Errors
                     .ToDictionary(
                         item => GetNormalizedKey(item.Key),
                         item => item.Value
                     ),
-                traceId = Activity.Current?.Id ?? context.HttpContext.TraceIdentifier
+                traceId = Activity.Current?.TraceId.ToString() ?? context.HttpContext.TraceIdentifier
             });
 
             context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;

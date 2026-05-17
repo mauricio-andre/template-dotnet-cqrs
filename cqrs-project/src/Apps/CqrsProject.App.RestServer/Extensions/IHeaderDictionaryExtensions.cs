e8584a1aadfcc4ee4d0e68a08f7885d987dc7840
mauricio.andre <mauricio.andre@becomex.com.br>
2026-05-17 12:37:49 -0300

    fix: ajustado regra do content-range

--- cqrs-project/src/Apps/CqrsProject.App.RestServer/Extensions/IHeaderDictionaryExtensions.cs
+++ cqrs-project/src/Apps/CqrsProject.App.RestServer/Extensions/IHeaderDictionaryExtensions.cs
@@ -7,16 +7,17 @@ public static class IHeaderDictionaryExtensions
     public static IHeaderDictionary AddContentRangeHeaders(
         this IHeaderDictionary headers,
         int? rangeStart,
-        int? rangeEnd,
+        int? takeRequired,
         int? size,
         string unit = "items")
     {
+        int? endRange = rangeStart ?? 0 + takeRequired;
         headers.Append("Content-Range", string.Concat(
             unit,
             " ",
             rangeStart ?? 0,
             "-",
-            rangeEnd ?? size ?? 0,
+            (endRange < size ? endRange : size) ?? 0,
             "/",
             size?.ToString() ?? "*"
         ));
